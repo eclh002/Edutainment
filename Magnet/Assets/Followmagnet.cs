682e15b099628386e2adfb092bdc0ab2425bc670
vedha gnanam <vetha.gnanam98@gmail.com>
2019-03-27 10:54:19 +0530

    added Unity Project

---
+++ Magnet/Assets/Followmagnet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Followmagnet : MonoBehaviour {
+
+	Transform tr_Player;
+	float f_RotSpeed=3.0f,f_MoveSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		tr_Player = GameObject.FindGameObjectWithTag ("Player").transform; }
+
+	// Update is called once per frame
+	void Update () {
+		/* Look at Player*/
+		transform.rotation = Quaternion.Slerp (transform.rotation , Quaternion.LookRotation (tr_Player.position - transform.position) , f_RotSpeed * Time.deltaTime);
+
+		/* Move at Player*/
+		transform.position += transform.forward * f_MoveSpeed * Time.deltaTime;
+	}
+
+	public void AdjustForce(float newForce){
+		f_MoveSpeed = newForce;
+	}
+}
---
+++ Magnet/Assets/MagneticForce.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var Magnet : GameObject;
+var forceFactor : float = 10;
+ 
+function Update () {
+ 
+ 
+ GetComponent.<Rigidbody>().AddForce((Magnet.transform.position - transform.position) * forceFactor * Time.smoothDeltaTime);
+ 
+ 
+}
