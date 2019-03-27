a28722c7c461dd6f6b65beb5f69313dcbf868068
vedha gnanam <vetha.gnanam98@gmail.com>
2019-03-27 20:37:22 +0530

    cleaned project
    converted script from js to c#
    removed Vuforia

---
+++ Magnet/Assets/MagneticForce.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+public class MagneticForce:MonoBehaviour{
+
+public GameObject Magnet ;
+public float forceFactor =10; 
+ 
+void Update () {
+ 
+ var force = (Magnet.transform.position - transform.position) * forceFactor * Time.smoothDeltaTime;
+GetComponent<Rigidbody>().AddForce(force);
+ 
+ 
+}
+
+
+
+
+}
